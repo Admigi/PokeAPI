7e0a7c8d378f02d199c37d996b6268552b3fcbd3
Clem <clement.duval44@hotmail.fr>
2026-01-27 00:12:52 +0900

    clean application file and updated Spring Boot to 3.5.10

--- src/main/java/com/pokemon/demo/poke_api/PokeApiApplication.java
+++ src/main/java/com/pokemon/demo/poke_api/PokeApiApplication.java
@@ -1,27 +1,15 @@
 package com.pokemon.demo.poke_api;
 
-import com.pokemon.demo.poke_api.provider.PokemonProvider;
-import com.pokemon.demo.poke_api.service.PokemonService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PokeApiApplication {
 
-//	@Autowired
-//	private static PokemonProvider provider;
-
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 
 		SpringApplication.run(PokeApiApplication.class, args);
-//		var ctx = SpringApplication.run(PokeApiApplication.class, args);
-//		var provider = ctx.getBean(PokemonProvider.class);
-//		var service =  ctx.getBean(PokemonService.class);
 
-//		System.out.println(provider.findAll());
-//		System.out.println(provider.findByName("Bulbasaur"));
-//		System.out.println(service.getAllPokemon());
 	}
 
 }
