b728a404246afd6f671e24899298a329ba0c7014
Clem <clement.duval44@hotmail.fr>
2026-04-08 19:29:34 +0900

    copy the woff2 files and write the @font-face rules in css file instead of using fontsource

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,10 +1,5 @@
 import { createRootRoute, HeadContent, Scripts } from "@tanstack/react-router";
 import Header from "../components/Header";
-import "@fontsource/rajdhani/400.css";
-import "@fontsource/rajdhani/500.css";
-import "@fontsource/rajdhani/600.css";
-import "@fontsource/rajdhani/700.css";
-import "@fontsource/rajdhani/900.css";
 import "../styles.css";
 
 export const Route = createRootRoute({
